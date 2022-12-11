37bbc6789afdca99e4cd0fb64ee0e0f1ba750e2b
Philip Trubitsyn <trph@bk.ru>
2022-12-11 22:44:08 +0300

    Первая задача из домашнего задания

---
+++ 1st/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int ReadLineInt()//Ввод числа INT
+{
+    int i = int.Parse(Console.ReadLine());
+    return i;
+}
+
+int [] MadeArray(int length)//Создание массива INT
+{
+    int [] array = new int [length];
+    return array;
+}
+
+int [] FillArrayInt(int [] array)//Заполнение массива
+{
+    for(int i = 0; i < array.Length; i++)
+    { 
+        array[i] = ReadLineInt();
+    }
+    return array;
+}
+
+Console.WriteLine($"Введите колличество чисел, которые Вы хотите ввести"); 
+int [] array = MadeArray(ReadLineInt());
+Console.WriteLine($"Вводите числа по одному через Enter");
+FillArrayInt(array);
+int sum = 0;
+for(int i = 0; i < array.Length; i++)
+{
+   if (array[i] > 0) 
+   sum++;
+}
+
+Console.WriteLine($"Колличество введенных чисел больше нуля {sum}");
+
+
+
+
