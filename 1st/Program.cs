55ecbcf71b046c1e19983f8b8e9bf554ec118492
Philip Trubitsyn <trph@bk.ru>
2022-12-12 13:08:43 +0300

    Небольшие исправления по ДЗ

--- 1st/Program.cs
+++ 1st/Program.cs
@@ -1,35 +1,35 @@
 ﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int ReadLineInt()//Ввод числа INT
+int ReadLineInt() // Ввод числа INT
 {
     int i = int.Parse(Console.ReadLine());
     return i;
 }
 
-int [] MadeArray(int length)//Создание массива INT
+int[] MadeArray(int length) // Создание массива INT
 {
-    int [] array = new int [length];
+    int[] array = new int[length];
     return array;
 }
 
-int [] FillArrayInt(int [] array)//Заполнение массива
+int[] FillArrayInt(int[] array) // Заполнение массива
 {
-    for(int i = 0; i < array.Length; i++)
-    { 
+    for (int i = 0; i < array.Length; i++)
+    {
         array[i] = ReadLineInt();
     }
     return array;
 }
 
-Console.WriteLine($"Введите количество чисел, которые Вы хотите ввести"); 
-int [] array = MadeArray(ReadLineInt());
+Console.WriteLine($"Введите количество чисел, которые Вы хотите ввести");
+int[] array = MadeArray(ReadLineInt());
 Console.WriteLine($"Вводите числа по одному через Enter");
 FillArrayInt(array);
 int sum = 0;
-for(int i = 0; i < array.Length; i++)
+for (int i = 0; i < array.Length; i++)
 {
-   if (array[i] > 0) 
-   sum++;
+    if (array[i] > 0)
+        sum++;
 }
 
 Console.WriteLine($"Количество введенных чисел больше нуля {sum}");
--- 2nd/Program.cs
+++ 2nd/Program.cs
@@ -7,36 +7,37 @@
 
 double ReadLineInt()//Ввод числа double
 {
-    double i = double.Parse(Console.ReadLine());
+    double i = double.Parse(Console.ReadLine()!);
     return i;
 }
 
-double [,] MadeArray2(int length,int length1)//Создание двумерного массива double
+double[,] MadeArray2(int length, int length1)//Создание двумерного массива double
 {
-    double [,] array = new double [length,length1];
+    double[,] array = new double[length, length1];
     return array;
 }
 
-double [,] FillArrayInt(double [,] array)//Заполнение двумерного массива
+double[,] FillArrayInt(double[,] array)//Заполнение двумерного массива
 {
-    for(int i = 0;  i < array.GetLength(0); i++)
-    { for(int y = 0; y < array.GetLength(1); y++)
-        array[i,y] = ReadLineInt();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = ReadLineInt();
     }
-       return array;
+    return array;
 }
 
-double [,] array = MadeArray2(2,2);
+double[,] array = MadeArray2(2, 2);
 Console.WriteLine($"Введите координаты точек b1, k1, b2 и k2 через ENTER");
-FillArrayInt (array);
-if(array[0,1] != array[1,1])
+FillArrayInt(array);
+if (array[0, 1] != array[1, 1])
 {
-double x = (array[1,0] - array[0,0]) / (array[0,1] - array[1,1]);// x = (b2 - b1) / (k1 - k2)
-double y = array[0,1] * x + array[0,0]; // y = k1 * x + b1
-Console.WriteLine($"Точка пересечения двух прямых находится на координатах x: {x} и у: {y}");
+    double x = (array[1, 0] - array[0, 0]) / (array[0, 1] - array[1, 1]);// x = (b2 - b1) / (k1 - k2)
+    double y = array[0, 1] * x + array[0, 0]; // y = k1 * x + b1
+    Console.WriteLine($"Точка пересечения двух прямых находится на координатах x: {x} и у: {y}");
 }
 else
-Console.WriteLine("Данные прямые параллельны и не пересекаются!");
+    Console.WriteLine("Данные прямые параллельны и не пересекаются!");
 
 
 
